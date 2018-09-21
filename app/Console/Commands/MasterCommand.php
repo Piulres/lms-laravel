5e6229747195cb51a8aaef16dc068c83349ea3bf
emigdio1 <renanemigdiogoncalves@hotmail.com>
2018-09-21 14:48:45 -0300

    21092018 - fix jaja cmds

---
+++ app/Console/Commands/MasterCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MasterCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'jaja:refresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Re-Run all bullshit';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // echo exec('php artisan serve');
+        echo ' Wait a minute doctor ';
+        echo ' > ';
+        echo exec('composer update');
+        echo exec('composer dump-autoload');
+        echo exec('php artisan migrate:fresh --seed');
+        echo exec('php artisan key:generate');
+        echo exec('php artisan vendor:publish --tag=lfm_config');
+        echo exec('php artisan vendor:publish --tag=lfm_public');
+        echo exec('php artisan cache:clear');
+        echo exec('clear');
+        echo ' Warlock of Darkness[8]: Done ';
+    }
+}
---
+++ app/Console/Commands/MasterCommand2.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MasterCommand2 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'jaja:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create all bullshit';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // echo exec('php artisan serve');
+        echo ' Wait a minute doctor ';
+        echo ' > ';
+        echo exec('composer install');
+        echo exec('composer dump-autoload');
+        echo exec('php artisan migrate --seed');
+        echo exec('php artisan key:generate');
+        echo exec('php artisan vendor:publish --tag=lfm_config');
+        echo exec('php artisan vendor:publish --tag=lfm_public');
+        echo exec('clear');
+        echo ' Warlock of Darkness[8]: Done ';
+    }
+}
