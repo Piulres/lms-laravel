d3ff4c0ed030d4ffdba9a7d61d658c608069eb2a
yama159 <fehiya@gmail.com>
2018-10-02 14:59:27 -0300

    02102018 - fixed data course

--- database/seeds/CourseLessonSeed.php
+++ database/seeds/CourseLessonSeed.php
@@ -13,11 +13,68 @@ class CourseLessonSeed extends Seeder
     {
         $faker = Faker\Factory::create();
 
-        for($i=0;$i<20;$i++){
+        /* for($i=0;$i<20;$i++){
             DB::table('course_lesson')->insert([
                 'course_id' => $faker->randomDigitNotNull,
                 'lesson_id' => $faker->randomDigitNotNull,
             ]);
-        }
+        } */
+
+        $items = [
+            ['course_id' => 1 , 'lesson_id' => 1,],
+            ['course_id' => 1 , 'lesson_id' => 2,],
+            ['course_id' => 1 , 'lesson_id' => 3,],
+            ['course_id' => 1 , 'lesson_id' => 4,],
+
+            ['course_id' => 2 , 'lesson_id' => 3,],
+            ['course_id' => 2 , 'lesson_id' => 4,],
+
+            ['course_id' => 3 , 'lesson_id' => 2,],
+            ['course_id' => 3 , 'lesson_id' => 3,],
+            ['course_id' => 3 , 'lesson_id' => 4,],
+            ['course_id' => 3 , 'lesson_id' => 5,],
+
+            ['course_id' => 4 , 'lesson_id' => 4,],
+            ['course_id' => 4 , 'lesson_id' => 5,],
+            ['course_id' => 4 , 'lesson_id' => 6,],
+            ['course_id' => 4 , 'lesson_id' => 7,],
+            ['course_id' => 4 , 'lesson_id' => 8,],
+            ['course_id' => 4 , 'lesson_id' => 9,],
+
+            ['course_id' => 5 , 'lesson_id' => 1,],
+            ['course_id' => 5 , 'lesson_id' => 3,],
+            ['course_id' => 5 , 'lesson_id' => 5,],
+
+            ['course_id' => 6 , 'lesson_id' => 1,],
+            ['course_id' => 6 , 'lesson_id' => 2,],
+            ['course_id' => 6 , 'lesson_id' => 3,],
+            ['course_id' => 6 , 'lesson_id' => 8,],
+
+            ['course_id' => 7 , 'lesson_id' => 3,],
+            ['course_id' => 7 , 'lesson_id' => 4,],
+            ['course_id' => 7 , 'lesson_id' => 5,],
+            ['course_id' => 7 , 'lesson_id' => 6,],
+            ['course_id' => 7 , 'lesson_id' => 7,],
+            ['course_id' => 7 , 'lesson_id' => 8,],
+            ['course_id' => 7 , 'lesson_id' => 9,],
+            ['course_id' => 7 , 'lesson_id' => 10,],
+
+            ['course_id' => 8 , 'lesson_id' => 2,],
+
+            ['course_id' => 9 , 'lesson_id' => 4,],
+            ['course_id' => 9 , 'lesson_id' => 8,],
+            ['course_id' => 9 , 'lesson_id' => 9,],
+
+            ['course_id' => 10 , 'lesson_id' => 1,],
+            ['course_id' => 10 , 'lesson_id' => 3,],
+            ['course_id' => 10 , 'lesson_id' => 5,],
+        ];
+
+        foreach ($items as $item) {
+            DB::table('course_lesson')->insert([
+                'course_id' => $item["course_id"],
+                'lesson_id' => $item["lesson_id"],
+            ]);
+        };
     }
 }
--- database/seeds/LessonsSeed.php
+++ database/seeds/LessonsSeed.php
@@ -13,7 +13,7 @@ class LessonsSeed extends Seeder
     {
         $faker = Faker\Factory::create();
        
-        for($i=0;$i<10;$i++){
+        /* for($i=0;$i<10;$i++){
             \App\Lesson::create([
                 'order' => $i+1,
                 'title' => $faker->word,
@@ -23,6 +23,75 @@ class LessonsSeed extends Seeder
                 'study_material' => $faker->imageUrl(300,300),
                 'status' => $faker->numberBetween(0,1),
             ]);
+        } */
+
+        $items = [
+            [   'order' => 1,
+                'title' => "aula 01",
+                'slug' => "aula1",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 2,
+                'title' => "aula 02",
+                'slug' => "aula2",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 3,
+                'title' => "aula 03",
+                'slug' => "aula3",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 4,
+                'title' => "aula 04",
+                'slug' => "aula4",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 5,
+                'title' => "aula 05",
+                'slug' => "aula5",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 6,
+                'title' => "aula 06",
+                'slug' => "aula6",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 7,
+                'title' => "aula 07",
+                'slug' => "aula7",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 8,
+                'title' => "aula 08",
+                'slug' => "aula8",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 9,
+                'title' => "aula 09",
+                'slug' => "aula9",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+            [   'order' => 10,
+                'title' => "aula 10",
+                'slug' => "aula10",
+                'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            ],
+
+        ];
+
+        foreach ($items as $item) {
+            \App\Lesson::create($item);
         }
+
     }
 }
