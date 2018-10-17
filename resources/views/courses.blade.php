37c98d18303a003f233842c30937fa397e2bb4e7
emigdio1 <renanemigdiogoncalves@hotmail.com>
2018-10-17 12:51:20 -0300

    demo

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -32,7 +32,7 @@
                 <div class="row">
 
                     <div class="col s12">
-                        <div class="ck">{!! $course->introduction !!}</div>                                       
+                        <div class="ck">{!! $course->description !!}</div>                                       
                     </div>
 
                 </div>
